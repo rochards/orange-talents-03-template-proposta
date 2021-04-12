d8825f98e899bda0d4fe1d26b30694bb950ac49e
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-04-12 11:02:50 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zupacademy/desafioproposta/DesafioPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.desafioproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/desafioproposta/DesafioPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.desafioproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
