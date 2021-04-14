7ec783989969777f69e357a1362cd7bbe88f72a9
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-04-14 09:32:47 -0300

    Add nova anotacao na interface CpfOrCnpj
    - A anotacao @Repeatable indica que a nossa anotacao @CpfOrCnpj
      pode ser repetida varias vezes em um mesmo local, dando a
      flexibilidade de configurar diferentes grupos de validacao, mensagens,
      etc.

--- src/main/java/br/com/zupacademy/desafioproposta/compartilhado/validators/CpfOrCnpj.java
+++ src/main/java/br/com/zupacademy/desafioproposta/compartilhado/validators/CpfOrCnpj.java
@@ -7,9 +7,11 @@ import org.hibernate.validator.constraints.br.CPF;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static br.com.zupacademy.desafioproposta.compartilhado.validators.CpfOrCnpj.*;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static org.hibernate.validator.constraints.CompositionType.OR;
@@ -24,6 +26,7 @@ import static org.hibernate.validator.constraints.CompositionType.OR;
 @Target({ FIELD, METHOD, PARAMETER, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = { })
+@Repeatable(List.class)
 public @interface CpfOrCnpj {
     String message() default "{br.com.zupacademy.desafioproposta.compartilhado.validators.CpfOrCnpj.message}";
 
