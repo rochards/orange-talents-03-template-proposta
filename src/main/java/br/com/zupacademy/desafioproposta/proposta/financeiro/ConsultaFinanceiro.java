716bbe2690af9e730759779166185deef5e34d59
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-04-14 17:34:44 -0300

    Add Health Check na API
    Alem das autoconfiguracoes de Health do Spring Boot Actuator, tbm adiciona um health check customizado para a integração com o servico de Analise Financeira;
    
    - Add dependencia do Spring Boot Actuator no pom.xml;
    - Add novo metodo na interface ConsultaFinanceiro;
    - Add classe FinanceiroHealthIndicator;
    - Add configuracoes do Actuator no arquivo application.properties

--- src/main/java/br/com/zupacademy/desafioproposta/proposta/financeiro/ConsultaFinanceiro.java
+++ src/main/java/br/com/zupacademy/desafioproposta/proposta/financeiro/ConsultaFinanceiro.java
@@ -2,10 +2,15 @@ package br.com.zupacademy.desafioproposta.proposta.financeiro;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(value = "solicitacaoPropostas", url = "${financeiro.url}")
 public interface ConsultaFinanceiro {
 
     @PostMapping
     SolicitacaoResponse analisaSolicitacaoDeProposta(SolicitacaoRequest solicitacaoRequest);
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    void isUp();
 }
---
+++ src/main/java/br/com/zupacademy/desafioproposta/proposta/financeiro/FinanceiroHealthIndicator.java
@@ -0,0 +1,30 @@
+package br.com.zupacademy.desafioproposta.proposta.financeiro;
+
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FinanceiroHealthIndicator implements HealthIndicator {
+
+    private final Logger logger = LoggerFactory.getLogger(FinanceiroHealthIndicator.class);
+    private final ConsultaFinanceiro consultaFinanceiro;
+
+    public FinanceiroHealthIndicator(ConsultaFinanceiro consultaFinanceiro) {
+        this.consultaFinanceiro = consultaFinanceiro;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            consultaFinanceiro.isUp();
+            return Health.up().build();
+        } catch (FeignException ex) {
+            logger.error(ex.getMessage());
+            return Health.down().build();
+        }
+    }
+}
