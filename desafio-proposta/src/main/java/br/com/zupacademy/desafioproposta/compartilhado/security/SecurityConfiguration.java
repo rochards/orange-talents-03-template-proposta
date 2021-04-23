097476fe46c7927740562c3248f8cb40523b5b40
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-04-23 09:09:14 -0300

    Login via senha cartao parte 1
    - Add dependencias de oauth2 no arquivo pom.xml;
    - Add classe SecurityConfiguration com matchers para os endpoints;
    - Add configuracoes do authorization server no arquivo application.properties

---
+++ desafio-proposta/src/main/java/br/com/zupacademy/desafioproposta/compartilhado/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.zupacademy.desafioproposta.compartilhado.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(GET, "/propostas/**").hasAuthority("SCOPE_propostas:read")
+                        .antMatchers(POST, "/propostas/**").hasAuthority("SCOPE_propostas:write")
+                        .antMatchers(POST, "/cartoes/**").hasAuthority("SCOPE_cartoes:write")
+                        .antMatchers(GET, "/actuator/**").hasAuthority("SCOPE_actuator:read")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
